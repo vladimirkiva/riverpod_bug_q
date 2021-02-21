77cc87e03df22a2a34327ad8d485b6143f8bbe70
vladimirkiva <vladimirkiva@gmail.com>
2021-02-21 14:08:53 +0300

    test simplification

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,55 +3,23 @@ import 'package:flutter_test/flutter_test.dart';
 
 // everything is simplified
 abstract class SuggestCompatibleState<T> {
-  int get count;
 }
 
 abstract class SuggestCompatibleModel<T, S extends SuggestCompatibleState<T>> extends StateNotifier<S> {
   SuggestCompatibleModel(SuggestCompatibleState<T> state) : super(state);
-
-  void add();
 }
 
 class User {
 }
 
 class UsersListState extends SuggestCompatibleState<User> {
-  Map<String, User> _users = Map();
-
-  @override
-  int get count => _users.length;
-
-  UsersListState copy() {
-    return UsersListState()
-      .._users = _users
-    ;
-  }
 }
 
 class UsersModel extends SuggestCompatibleModel<User, UsersListState> {
   UsersModel() : super(UsersListState());
-
-  @override
-  void add() {
-    state._users[state._users.length.toString()] = User();
-    state = state;
-  }
-
-  int value() {
-    return state.count;
-  }
-}
-
-class Refresher extends StateNotifier<int> {
-  Refresher() : super(0);
-
-  void refresh() {
-    state = state++;
-  }
 }
 
 final usersProvider = StateNotifierProvider<UsersModel>((_) => UsersModel());
-final refreshProvider = StateNotifierProvider<Refresher>((_) => Refresher());
 
 class MockWidget<T> {
   final StateNotifierProvider<SuggestCompatibleModel<T, SuggestCompatibleState<T>>> _provider;
